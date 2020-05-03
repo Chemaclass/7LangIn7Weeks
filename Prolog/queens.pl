d7506e00efa45826e7e42e6f3fa1601c38ce7989
Chemaclass <chemaclass@outlook.es>
2020-05-03 11:53:35 +0200

    optimized quees.pl

--- Prolog/queens.pl
+++ Prolog/queens.pl
@@ -1,19 +1,8 @@
 % The queens problem
 
-valid_queen((Row, Col)) :-
-  Range = [1,2,3,4,5,6,7,8],
-  member(Row, Range),
-  member(Col, Range).
-
+valid_queen((Row, Col)) :- member(Col, [1,2,3,4,5,6,7,8]).
 valid_board([]).
-valid_board([Head|Tail]) :- 
-  valid_queen(Head),
-  valid_board(Tail).
-
-% rows(Queens, Rows)
-rows([], []).
-rows([(Row, _)|QueensTail], [Row|RowsTail]) :-
-  rows(QueensTail, RowsTail).
+valid_board([Head|Tail]) :- valid_queen(Head), valid_board(Tail).
 
 % cols(Queens, Cols)
 cols([], []).
@@ -33,15 +22,12 @@ diags2([(Row, Col)|QueensTail], [Diagonal, DiagonalsTail]) :-
   diags2(QueensTail, DiagonalsTail).
 
 eight_queens(Board) :- 
-  length(Board, 8),
+  Board = [(1, _), (2, _), (3, _), (4, _), (5, _), (6, _), (7, _), (8, _)],
   valid_board(Board),
 
-  rows(Board, Rows),
   cols(Board, Cols),
   diags1(Board, Diags1),
   diags2(Board, Diags2),
-
-  fd_all_different(Rows),
   fd_all_different(Cols),
   fd_all_different(Diags1),
   fd_all_different(Diags2).
