4933894f9378a89e6dac8d3acfb8df54838ff737
Chemaclass <chemaclass@outlook.es>
2020-05-21 22:22:46 +0200

    Haskell: lazy sequence of prime numbers

--- Haskell/exercices.hs
+++ Haskell/exercices.hs
@@ -6,5 +6,12 @@ qsort (h:tail) = (qsort lesser) ++ [h] ++ (qsort greater)
   where 
     lesser = filter (< h) tail
     greater = filter (>= h) tail
-
 -- Example: qsort [5,6,1,89,2,3,1] => [1,1,2,3,5,6,89]
+
+---------------------------------------------------------------
+-- Create a lazy sequence of prime numbers
+lazyPrime = [n | n <- [1..], isPrime n]
+  where 
+    isPrime n = all ((> 0) . rem n) [2..n-1]
+-- Example: take 10 lazyPrime => [1,2,3,5,7,11,13,17,19,23]
+
