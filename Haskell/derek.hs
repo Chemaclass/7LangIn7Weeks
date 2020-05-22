32ab690a6c1b7bb41c12841b2eb3012d8b46d159
Chemaclass <chemaclass@outlook.es>
2020-05-22 16:19:38 +0200

    Haskell: classes

--- Haskell/derek.hs
+++ Haskell/derek.hs
@@ -216,3 +216,46 @@ area (Rectangle x y x2 y2) = (abs $ x2-x) * (abs(y2-y))
 
 areaOfCircle = area (Circle 50 60 20)
 areaOfRect = area $ Rectangle 10 10 100 100
+
+-- type classes
+-- Num Eq Or Show ...
+
+data Employee = Employee { 
+  name :: String,
+  position  :: String,
+  idNum :: Int
+} deriving (Eq, Show)
+
+samSmith = Employee {name="Sam Smith", position="Manager",idNum=100}
+pamMarx = Employee {name="Pam Marx", position="Sales",idNum=101}
+
+isSamPam = samSmith == pamMarx
+samSmithData = show samSmith
+
+data ShirtSize = S | M | L
+
+instance Eq ShirtSize where
+  S == S = True
+  M == M = True
+  L == L = True
+  _ == _ = False
+
+instance Show ShirtSize where
+  show S = "Small"
+  show M = "Medium"
+  show L = "Large"
+
+smallAvail = elem S [S, M, L]
+theSize = show S 
+
+
+class MyEq a where 
+  areEqual :: a -> a -> Bool 
+
+instance MyEq ShirtSize where
+  areEqual S S = True
+  areEqual M M = True
+  areEqual L L = True
+  areEqual _ _ = False
+
+newSize = areEqual M M
