9c6fb1a765ae37f5a177575967802677895e61a9
Chemaclass <chemaclass@outlook.es>
2020-05-23 19:15:12 +0200

    Haskell: following course for imperative programmers

--- Haskell/tut_for_imperative_prog.hs
+++ Haskell/tut_for_imperative_prog.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 ------------------------------------
 --------- List exercices -----------
 ------------------------------------
@@ -48,3 +50,89 @@ hasPath' xs x y
     let xs' = [(n,m) | (n,m) <- xs, n /=m ] in
     or [hasPath' xs' m y | (n,m) <- xs, n == x]
 
+------------------------------------
+--------- Datatypes ----------------
+------------------------------------
+
+data Calculation =
+  Add Int Int | Sub Int Int | Mul Int Int | Div Int Int
+
+calc :: Calculation -> Int
+calc (Add x y) = x + y
+calc (Sub x y) = x - y
+calc (Mul x y) = x * y
+calc (Div x y) = x `div` y
+
+data PeaNum = Succ PeaNum | Zero
+four :: PeaNum
+four = Succ $ Succ $ Succ $ Zero
+
+data Tree a = Leaf | Node (Tree a) a (Tree a)
+tree :: Tree Int 
+tree = 
+  Node (Node Leaf 1 Leaf) 2 (Node (Node Leaf 3 Leaf) 4 Leaf)
+
+
+------------------------------------
+------- Folding exercices ----------
+------------------------------------
+{- #1
+Create a function rev' that reverses a list
+-}
+rev' :: [a] -> [a]
+rev' = foldl (\acc x -> x : acc) []
+-- OR => rev' = foldl (flip(:)) []
+
+{- #2
+Create a function prefixes that returns all
+prefixes of a given list
+-}
+prefixes' :: [a] -> [[a]]
+prefixes' = foldr (\x acc -> [x] : (map ((:) x) acc)) []
+-- prefixes [1,2,3] => [[1],[1,2],[1,2,3]]
+
+
+------------------------------------
+------- IO -------------------------
+------------------------------------
+
+greet :: IO ()
+greet = do
+  putStrLn "What is your name?"
+  name <- getLine
+  let uname = map toUpper name
+  putStrLn ("Hello " ++ uname ++ ".")
+
+main' :: IO ()
+main' = do
+  line <- getLine
+  if line /= "quit" then do
+    putStrLn ("Input: "++line)
+    main'
+  else 
+    return ()
+ 
+
+------------------------------------
+------- Mondads --------------------
+--------- and the DO notation ------
+
+monadd :: (Monad m, Num b) => m b -> m b -> m b
+-- monadd mx my = 
+--   mx >== (\x -> my >>= (\y -> return $ x+y))
+monadd mx my = do
+  x <- mx 
+  y <- my 
+  return $ x+y
+-- Example: monadd [1] [2] => [3]
+{-
+# Monad Laws
+## Left identity
+return a >>= k = k a
+
+## Right identity
+m >>= return = m
+
+## Associativity
+m >>= (\x -> k x >>= h) = (m >> k) >>= h
+-}
